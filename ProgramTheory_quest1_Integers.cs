207bb4cc0c0fa79d2779019b064cacbb3d533eac
adtautavel <64550484+adtautavel@users.noreply.github.com>
2020-09-28 11:59:23 +0200

    Create ProgramTheory_quest1_Integers.cs

---
+++ ProgramTheory_quest1_Integers.cs
@@ -0,0 +1,12 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+    int x = int.MaxValue;
+    long y = long.MaxValue;
+    short z = short.MaxValue;
+
+    long sum = x+y+z;
+  }
+ }
