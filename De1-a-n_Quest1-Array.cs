791aadf0fc8b89caa9e13e411554d2f8de11456f
adtautavel <64550484+adtautavel@users.noreply.github.com>
2020-10-05 11:50:40 +0200

    Update De1-a-n_Quest1-Array.cs

--- De1-a-n_Quest1-Array.cs
+++ De1-a-n_Quest1-Array.cs
@@ -10,21 +10,16 @@ class MainClass {
     stringArray[1]="Petros";
     stringArray2[0]="Eddy";
     stringArray2[1]="Raul";
-  
-  for (int i =0; i<stringArray3.Length;i++)
-    {
-    if (i<stringArray.Length)
-      {
-      stringArray3[i]=stringArray[i];
-      Console.WriteLine($"stringArray3[{i}]={stringArray3[i]}");
-      }
-      else
-      {
-        stringArray3[i]=stringArray2[i-2];
-        Console.WriteLine($"stringArray3[{i}]={stringArray3[i]}");
-      }    
-    }
     
+    stringArray.CopyTo(stringArray3,0);
+    stringArray2.CopyTo(stringArray3,stringArray2.Length);
+    
+    for (int i =0; i<stringArray3.Length;i++)
+      {
+     
+      Console.WriteLine($"stringArray3[{i}]={stringArray3[i]}");
+     }
+     
   
 
   }
